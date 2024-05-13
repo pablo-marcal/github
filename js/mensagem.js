a0c25ffb49ff99962fafe473348fc597e2bcf1d8
unknown <helievysonp@gmail.com>
2024-05-13 13:27:57 -0300

    primeiro

---
+++ js/mensagem.js
@@ -0,0 +1,26 @@
+// let msg=("olá mundo")
+
+// let nome= prompt("qual seu nome: ")
+
+// let n1=+prompt("digite o número:")
+// let n2=+prompt("digite o número:")
+// let soma = n1+n2
+// let sub = n1-n2
+// let mult = n1*n2
+// let div = n1/n2
+// alert(msg)
+// document.write(soma + "<br>")
+// document.write(sub + "<br>")
+// document.write(mult + "<br>")
+// document.write(div + "<br>")
+
+// for(i = 50; i >=0; i--){
+//     if(i % 2 != 0)
+//     document.write(i+"<br/>")
+//   }
+
+
+let nome = prom
+
+
+       
